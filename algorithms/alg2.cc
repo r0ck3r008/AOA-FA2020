b29151bad126ab723d9cd5160590893f9a345e5b
r0ck3r008 <naman.arora@tutanota.com>
2020-11-18 02:05:42 -0500

    Return void and fill up pairs of co-ords ret

--- algorithms/alg2.cc
+++ algorithms/alg2.cc
@@ -4,25 +4,34 @@
 
 using namespace algorithms::alg2;
 
-int
-run(vector<vector<int>> &mat, int h)
+void
+task3(vector<vector<int>> &mat, int h, pair<pair<int, int>, pair<int, int>> &ret)
 {
 	int max_sz = 0;
-	for(int i = 0; i < mat.size(); i++) {
+        int ret_pairs[4];
+	for(uint32_t i = 0; i < mat.size(); i++) {
 		int flag = 1;
-		for(int j = i; j < mat[i].size(); j++) {
-			for(int k = j; k < mat.size() && flag; k++) {
-				for(int l = i; i < mat[i].size() && flag; l++) {
+		for(uint32_t j = i; j < mat[i].size(); j++) {
+                        uint32_t k = j, l = i;
+			for(; k < mat.size() && flag; k++) {
+				for(; i < mat[i].size() && flag; l++) {
 					if(mat[k][j] < h)
 						flag = 0;
 				}
 			}
 			if(flag) {
 				int sz = (i + 1) * (j + 1);
-				if (max_sz < sz)
+				if (max_sz < sz) {
 					max_sz = sz;
+                                        ret_pairs[0] = k;
+                                        ret_pairs[1] = l;
+                                        ret_pairs[2] = i;
+                                        ret_pairs[3] = j;
+                                }
 			}
 		}
 	}
-	return max_sz;
+
+        ret.first = pair<int, int>(ret_pairs[0], ret_pairs[1]);
+        ret.second = pair<int, int>(ret_pairs[2], ret_pairs[3]);
 }
