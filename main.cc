c26b6e46f1557abf9513d45bb8dae6f982baf9ef
r0ck3r008 <naman.arora@tutanota.com>
2020-11-25 16:35:25 -0500

    Define and open global pointer to log file

--- main.cc
+++ main.cc
@@ -1,6 +1,24 @@
 #include<iostream>
+#include<string.h>
+#include<unistd.h>
+#include<errno.h>
 
-int
-main(int argc, char **argv)
+const FILE *logfile;
+
+int main(int argc, char **argv)
 {
+        if(argc!=2) {
+                fprintf(stderr, "[!]Usage: ./AlgoTowers <logfile_name>\n");
+                _exit(1);
+        }
+
+        FILE *f=NULL;
+        if((f=fopen(argv[1], "r")) == NULL) {
+                fprintf(stderr, "[-]Error in opening: %s: %s\n", argv[1], strerror(errno));
+                _exit(1);
+        }
+
+        logfile = f;
+
+        fclose(f);
 }
