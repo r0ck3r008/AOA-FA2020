4873efab2d3d56d9772701e12cfbc7eb6adc683b
r0ck3r008 <naman.arora@tutanota.com>
2020-11-25 17:59:29 -0500

    Logfile is optional and input file is required

--- main.cc
+++ main.cc
@@ -3,22 +3,29 @@
 #include<unistd.h>
 #include<errno.h>
 
-const FILE *logfile;
+static FILE *logfile = stdout;
 
 int main(int argc, char **argv)
 {
-        if(argc!=2) {
-                fprintf(stderr, "[!]Usage: ./AlgoTowers <logfile_name>\n");
+        if(argc<2) {
+                fprintf(stderr, "[!]Usage: ./AlgoTowers <input_file> [<logfile_name>]\n");
+                _exit(1);
+        } else if(argc==3) {
+                FILE *logf=NULL;
+                if((logf=fopen(argv[2], "w")) == NULL) {
+                        fprintf(stderr, "[-]Fopen: %s: %s\n", argv[2], strerror(errno));
+                        _exit(1);
+                }
+                logfile=logf;
+        }
+
+        FILE *inf=NULL;
+        if((inf=fopen(argv[1], "r")) == NULL) {
+                fprintf(stderr, "[-]Fopen: %s: %s\n", argv[1], strerror(errno));
                 _exit(1);
         }
 
-        FILE *f=NULL;
-        if((f=fopen(argv[1], "r")) == NULL) {
-                fprintf(stderr, "[-]Error in opening: %s: %s\n", argv[1], strerror(errno));
-                _exit(1);
-        }
 
-        logfile = f;
-
-        fclose(f);
+        fclose(inf);
+        fclose(logfile);
 }
