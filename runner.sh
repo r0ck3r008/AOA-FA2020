0086f354df7b00f25212e405af5d47ac924e2e95
r0ck3r008 <naman.arora@tutanota.com>
2020-11-25 17:37:04 -0500

    Runner now executes binary and stores in data.csv

--- runner.sh
+++ runner.sh
@@ -2,9 +2,22 @@
 
 # Generate 4 input files
 i=1
+if [ -n "$1" ];
+then
+        while [ $i -le 4 ];
+        do
+                val=$((10 ** $i))
+                python ./tc_generate.py "$val" testcases/tc"$val".txt
+                i=$(( $i + 1 ))
+        done
+fi
+
+i=1
 while [ $i -le 4 ];
 do
         val=$((10 ** $i))
-        python ./tc_generate.py "$val" testcases/tc"$val".txt
+        ./AlgoTowers testcases/tc"$val".txt | awk -v="$val" 'print{val,$0}' >> tmp/data.csv
         i=$(( $i + 1 ))
 done
+
+sort -t, -k1 tmp/data.csv
