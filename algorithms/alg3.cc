ac317839a964972ecb287ac7a61d0f3e47eb8a8d
r0ck3r008 <naman.arora@tutanota.com>
2020-11-25 15:48:00 -0500

    Algorithm 3, tasks 4 and 5

---
+++ algorithms/alg3.cc
@@ -0,0 +1,21 @@
+#include<iostream>
+
+#include"algorithms/alg3.h"
+
+using namespace algorithms::alg3;
+
+int
+task4(vector<vector<int>> &mat, int h)
+{
+	int max_sz = 0;
+
+	return max_sz;
+}
+
+int
+task5(vector<vector<int>> &mat, int h)
+{
+	int max_sz = 0;
+
+	return max_sz;
+}
---
+++ include/algorithms/alg3.h
@@ -0,0 +1,15 @@
+#ifndef ALG3_H
+#define ALG3_H
+
+#include<vector>
+
+using std::vector;
+
+namespace algorithms {
+	namespace alg3 {
+		int task4(vector<vector<int>> &, int);
+		int task5(vector<vector<int>> &, int);
+	}
+}
+
+#endif
