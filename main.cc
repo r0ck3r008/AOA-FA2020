3eafc681417b71dfc1d016b8fa279d46d5874951
r0ck3r008 <naman.arora@tutanota.com>
2020-11-25 17:59:52 -0500

    Function to read input file and height

--- main.cc
+++ main.cc
@@ -1,9 +1,41 @@
 #include<iostream>
+#include<vector>
+#include<stdio.h>
+#include<string.h>
 #include<string.h>
 #include<unistd.h>
 #include<errno.h>
 
+using std::vector;
+
 static FILE *logfile = stdout;
+
+void input_tc(vector<vector<int>> &grid, int *height, FILE *f)
+{
+        char *line=NULL;
+        size_t n=0;
+        bool flag=true;
+        int rows, cols;
+        while(getline(&line, &n, f)) {
+                char buf[2048];
+                strncpy(buf, line, 2048 * sizeof(char));
+                if(flag) {
+                        rows = strtol(strtok(buf, " "), NULL, 10);
+                        cols = strtol(strtok(NULL, " "), NULL, 10);
+                        *height = strtol(strtok(NULL, " "), NULL, 10);
+                        flag=false;
+                } else {
+                        vector<int> column(cols);
+                        column.push_back(strtol(strtok(buf, " "), NULL, 10));
+                        for(int i=0; i<cols-1; i++)
+                                column.push_back(strtol(strtok(NULL, " "), NULL, 10));
+                        grid.push_back(column);
+                }
+                free(line);
+                line=NULL;
+                n=0;
+        }
+}
 
 int main(int argc, char **argv)
 {
@@ -25,6 +57,9 @@ int main(int argc, char **argv)
                 _exit(1);
         }
 
+        vector<vector<int>> grid;
+        int height=0;
+        input_tc(grid, &height, inf);
 
         fclose(inf);
         fclose(logfile);
