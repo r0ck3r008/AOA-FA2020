41df156f9540059917a32f59a2eeddf7cf1dd703
r0ck3r008 <naman.arora@tutanota.com>
2020-11-27 17:08:57 -0500

    Implemented task5

--- algorithms/alg3.cc
+++ algorithms/alg3.cc
@@ -3,19 +3,72 @@
 #include"algorithms/alg3.h"
 
 using namespace algorithms::alg3;
+using std::max;
 
 int
-algorithms::alg3::task4(vector<vector<int>> &mat, int h, int *ret)
+algorithms::alg3::task4(vector<vector<int>> &mat, int *ret)
 {
-	int max_sz = 0;
+        int max_sz = 0;
 
-	return max_sz;
+        return max_sz;
 }
 
-int
-algorithms::alg3::task5(vector<vector<int>> &mat, int h, int *ret)
+void
+rowisemax(vector<int> &height, int row, int *max_sz, int *ret)
 {
-	int max_sz = 0;
+        vector<int> s;
+        vector<int> left(height.size(), 0);
+        vector<int> right(height.size(), 0);
 
-	return max_sz;
+        for(uint32_t i=0; i<height.size(); i++) {
+                while(!s.empty() && height[i] <= height[s[s.size() - 1]])
+                        s.pop_back();
+                if(s.empty())
+                        left[i] = -1;
+                else
+                        left[i] = s[s.size() - 1];
+                s.push_back(i);
+        }
+
+        s.clear();
+
+        for(int i=height.size()-1; i>=0; i--) {
+                while(!s.empty() && height[i] <= height[s[s.size() - 1]])
+                        s.pop_back();
+                if(s.empty())
+                        right[i] = height.size();
+                else
+                        right[i] = s[s.size() - 1];
+                s.push_back(i);
+        }
+
+
+        int area, width;
+        for(uint32_t i=0; i<height.size(); i++) {
+                width = right[i] - left[i] - 1;
+                area = height[i] * width;
+                if(area > *max_sz) {
+                        *max_sz = area;
+                        ret[0] = row - height[i] + 1;
+                        ret[1] = left[i] + 1;
+                        ret[2] = row;
+                        ret[3] = right[i] - 1;
+                }
+        }
+}
+
+void
+algorithms::alg3::task5(vector<vector<int>> &mat, int *ret)
+{
+        int max_sz = 0;
+        vector<int> height(mat[0].size(), 0);
+        for(uint32_t i=0; i<mat.size(); i++) {
+                for(uint32_t j=0; j<mat[i].size(); j++) {
+                        if(mat[i][j])
+                                height[j]++;
+                        else
+                                height[j]=0;
+                }
+                rowisemax(height, i, &max_sz, ret);
+        }
 }
