f51be5d61effd31819b084fc1fd9e38f384a1bc8
r0ck3r008 <naman.arora@tutanota.com>
2020-11-27 02:05:44 -0500

    Fixed tasks 1 and 2

--- algorithms/alg1.cc
+++ algorithms/alg1.cc
@@ -9,66 +9,72 @@ using std::max;
 using std::min;
 
 int
-topdown(vector<vector<int>> &mat, uint32_t i, uint32_t j, int h, vector<vector<int>> &cache)
+topdown(vector<vector<int>> &mat, uint32_t i, uint32_t j, vector<vector<int>> &cache)
 {
         if(i>=mat.size() || j>=mat[i].size())
                 return 0;
 
-        if(mat[i][j]<h) {
+        if(!mat[i][j]) {
                 cache[i][j] = 0;
         } else if(cache[i][j]==-1) {
-                cache[i][j] = min(topdown(mat, i+1, j, h, cache),
-                                min(topdown(mat, i, j+1, h, cache),
-                                        topdown(mat, i+1, j+1, h, cache))) + 1;
+                cache[i][j] = min(topdown(mat, i+1, j, cache),
+                                min(topdown(mat, i, j+1, cache),
+                                        topdown(mat, i+1, j+1, cache))) + 1;
         }
 
         return cache[i][j];
 }
 
 void
-algorithms::alg1::task1(vector<vector<int>> &mat, int h, int *ret)
+algorithms::alg1::task1(vector<vector<int>> &mat, int *ret)
 {
         vector<vector<int>> cache(mat.size(), vector<int>(mat[0].size(), -1));
         int max_sz = 0;
-        bool flag = false;
         for(uint32_t i=0; i<mat.size(); i++) {
                 for(uint32_t j=0; j<mat[i].size(); j++) {
-                        int tmp_max = topdown(mat, i, j, h, cache);
+                        int tmp_max = topdown(mat, i, j, cache);
                         if(tmp_max > max_sz) {
                                 max_sz = tmp_max;
-                                flag=true;
                                 ret[0] = i;
                                 ret[1] = j;
                         }
                 }
         }
 
-        if(flag) {
+        if(max_sz) {
                 ret[2] = max_sz + ret[0] - 1;
                 ret[3] = max_sz + ret[1] - 1;
         }
 }
 
 void
-algorithms::alg1::task2(vector<vector<int>> &mat, int h, int *ret)
+algorithms::alg1::task2(vector<vector<int>> &mat, int *ret)
 {
 	int max_sz = 0;
-        vector<vector<int>> dp(3, vector<int>(mat[0].size()+1, 0));
+        vector<vector<int>> dp(2, vector<int>(mat[0].size()+1, 0));
         for(uint32_t i=0; i<mat.size(); i++) {
                 for(uint32_t j=0; j<mat[i].size(); j++) {
-                        int new_i = i%2+1, new_j = j%2+1;
-                        if(mat[i][j] >= h) {
-                                dp[new_i][new_j] = 1 + min(dp[new_i-1][new_j],
-                                                min(dp[new_i][new_j-1],
-                                                dp[new_i-1][new_j-1]));
+                        int new_i = i%2, new_j = j+1;
+                        if(mat[i][j]) {
+                                int offset = -1;
+                                if(new_i == 0)
+                                        offset = 1;
+                                dp[new_i][new_j] = 1 + min(dp[new_i+offset][new_j],
+                                                        min(dp[new_i][new_j-1],
+                                                                dp[new_i+offset][new_j-1]));
                                 if(dp[new_i][new_j] > max_sz) {
                                         max_sz = dp[new_i][new_j];
-                                        ret[0] = i;
-                                        ret[1] = j;
+                                        ret[2] = i;
+                                        ret[3] = j;
                                 }
                         } else {
                                 dp[new_i][new_j] = 0;
                         }
                 }
+        }
+
+        if(max_sz) {
+                ret[0] = ret[2] - max_sz + 1;
+                ret[1] = ret[3] - max_sz + 1;
         }
 }
